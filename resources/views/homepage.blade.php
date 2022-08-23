af1b3a01b6893c3a628030cf0fa12ecf6f0dd686
Tereschenko Tanya <trschnkt@gmail.com>
2022-08-23 13:01:49 +0300

    Added MyApp

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title', 'Homepage')
+
+@section('content')
+    <div class="px-4 py-5 my-5 text-center">
+        <h1 class="display-5 fw-bold">Simple Greeting!</h1>
+    </div>
+@endsection
